c2a3a4123cf1f286300edb24c7813af489cf442e
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-06 11:46:38 +0530

    Add files via upload

---
+++ Day 3 - Datatype Conversion/script.js
@@ -0,0 +1,22 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn = "Om";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 14;
+
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+console.log(stringNumber);
