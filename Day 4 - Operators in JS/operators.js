6fc7b3e6fa63e3b89f6c41bca5d3f0bb03bcf82b
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-07 22:49:50 +0530

    Add files via upload

---
+++ Day 4 - Operators in JS/operators.js
@@ -0,0 +1,38 @@
+let x = 3;
+let y = 2;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x ** y);
+// console.log(x / y);
+// console.log(x % y);
+
+x = y;
+x += y;
+x -= y;
+x *= y;
+x **= y;
+x /= y;
+x %= y;
+
+// console.log(x);
+
+let age = 18;
+
+// age == 18;
+// age != 18;
+// age === 18;
+// age !== 18;
+// age > 18;
+// age < 18;
+// age >= 18;
+// age <= 18;
+// age ? 18;
+
+if (age != 18) {
+    console.log("You can drive");
+}
+else {
+    console.log("You cannot drive");
+}
---
+++ Day 4 - Operators in JS/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "Hello";
+let str2 = " Om";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+let x = 3;
+const y = ++x;
+
+console.log(x, y);
+
+let a = 3;
+const b = a++;
+
+console.log(a, b);
