5bcc7c6150cea4ba2a8908264fc5d747bed8ef9b
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-10 21:33:41 +0530

    Add files via upload

---
+++ Day 7 - Stack and Heap memory/script.js
@@ -0,0 +1,19 @@
+let myYoutubeName = "codewithom";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "omgaikwad14@gmail.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "om14@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
