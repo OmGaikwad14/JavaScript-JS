c51960d7617dfe962f9d9d8c3b2634e7863e09e2
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-16 12:23:05 +0530

    Update code

--- Day 8 - Strings/script.js
+++ Day 8 - Strings/script.js
@@ -1,37 +1,35 @@
 const name = "Vishal";
-const repoCount = 14;
+const repoCount = 50;
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Volly-ball');
+const gameName = new String('Volly-Ball-Game');
 
-console.log(gameName[0]);
+console.log(gameName);
+// console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf('y'));
-
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('o'));
 
 const newString = gameName.substring(0, 4);
 console.log(newString);
 
-const anotherString = gameName.slice(-9, 4)
+const anotherString = gameName.slice(-9, 10)
 console.log(anotherString);
 
-const newStringOne = "  Vishal  ";
-console.log(newStringOne);
-console.log(newStringOne.trim());
+const newString1 = "  Vishal  ";
+console.log(newString1);
+console.log(newString1.trim());
 
-const url = "https://www.youtube.com/Om%14gaikwad";
+const url = "https://vishal.com/vishal%18babar";
 
-console.log(url.replace('%14', '-'));
-
-console.log(url.includes('Om'));
-
+console.log(url.replace('%18', '-'));
+console.log(url.includes('vishal'));
 
 console.log(gameName.split('-'));
