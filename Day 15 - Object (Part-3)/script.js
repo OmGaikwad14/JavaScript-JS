f6bf963d89815552ff6534ab4d0ecb297911810d
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-20 12:52:03 +0530

    Add files via upload

---
+++ Day 15 - Object (Part-3)/script.js
@@ -0,0 +1,27 @@
+const course = {
+    courseName: "JS in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// console.log(course.courseInstructor);
+
+
+const { courseInstructor: instructor } = course
+console.log(instructor);
+
+
+// JSON API - (Application Programming Interface)
+
+
+// {
+//     "name": "Vishal",
+//         "courseName": "JS in Hindi",
+//             "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
