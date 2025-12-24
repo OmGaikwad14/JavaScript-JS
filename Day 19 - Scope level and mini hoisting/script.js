f50c3990a9cda8d16605a268765496cb1329457e
Om Gaikwad <omgaikwad140@gmail.com>
2025-12-24 23:48:01 +0530

    Add files via upload

---
+++ Day 19 - Scope level and mini hoisting/script.js
@@ -0,0 +1,42 @@
+function one() {
+    const username = "Vishal"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Vishal"
+    if (username === "Vishal") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++++ Interesting +++++++++++++++++++++
+
+console.log(addone(5));
+
+
+function addone(num) {
+    return num + 1
+}
+
+
+console.log(addTwo(5));
+
+const addTwo = function (num) {
+    return num + 2
+}
