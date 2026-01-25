486e8ad05cccb9ebe8355a245c18b57fa483ee83
Om Gaikwad <omgaikwad140@gmail.com>
2026-01-25 23:41:47 +0530

    Fetch user data from GitHub API

--- Day 38 - Fetch/script.js
+++ Day 38 - Fetch/script.js
@@ -1,1 +1,8 @@
-
+fetch('https://api.github.com/users/OmGaikwad14')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => console.log(error));
