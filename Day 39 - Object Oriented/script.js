0f26020a9271b66cf32b35b8e43ab50c70091a4f
Om Gaikwad <omgaikwad140@gmail.com>
2026-01-26 12:22:17 +0530

    Implement User constructor and create user instances

--- Day 39 - Object Oriented/script.js
+++ Day 39 - Object Oriented/script.js
@@ -1,1 +1,49 @@
+const user = {
+    username: "Om",
+    loginCount: 8,
+    signedIn: true,
 
+    getUserDetails: function () {
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+    }
+}
+
+const user2 = {
+    username: "Om",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function () {
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+    }
+}
+
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function () {
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this
+}
+
+const userOne = new User("Om", 12, true)
+const userTwo = new User("Vishal", 11, false)
+
+console.log(userOne.constructor);
+// console.log(userTwo);
