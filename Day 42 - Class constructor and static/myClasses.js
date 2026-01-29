8f263ecf6a0b301fc53b792da9714f8321c81abd
Om Gaikwad <omgaikwad140@gmail.com>
2026-01-29 23:52:02 +0530

    Refactor User class to use constructor function

--- Day 42 - Class constructor and static/myClasses.js
+++ Day 42 - Class constructor and static/myClasses.js
@@ -1,1 +1,46 @@
+// ES6
 
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword() {
+//         return `${this.password}chai`
+//     }
+//     changeUsername() {
+//         return `${this.username.toUpperCase()}`
+//     }
+
+
+// }
+
+// const chai = new User('chai', 'chai@gmail.com', '1234')
+
+// console.log(chai.changeUsername());
+// console.log(chai.encryptPassword());
+
+
+// behind the scene
+
+function User(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function () {
+    return `${this.password}tea`
+}
+
+User.prototype.changeUsername = function () {
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User('tea', 'tea@gmail.com', '1712')
+
+console.log(tea.changeUsername());
+console.log(tea.encryptPassword());
